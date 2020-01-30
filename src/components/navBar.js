6fa69b78b4b3c68d10fcb2a8854a02ab1851fcf2
valeravilks <valeravilks@gmail.com>
2020-01-30 11:55:11 +0300

    Header in styled component

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import {Navbar, Container} from "react-bootstrap";
+import {Container} from "react-bootstrap";
+import styled from 'styled-components';
+
+let AppBar = styled.div`
+    box-shadow: 0px -4px 20px rgba(0, 0, 0, 0.25);
+`;
+
+let MainText = styled.div`
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+`;
+
+let CustomContainer = styled(Container)`
+    display: flex;
+    align-items: center;
+    min-height: 70px;
+    margin-bottom: 40px;
+`;
 
 export default class extends React.Component{
     render() {
         return(
-            <Navbar className='mb-5' expand="lg" variant="light" bg="light">
-                <Container>
-                    <Navbar.Brand>Тестовое задание</Navbar.Brand>
-                </Container>
-            </Navbar>
+            <AppBar>
+                <CustomContainer>
+                    <MainText>Тестовое задание</MainText>
+                </CustomContainer>
+            </AppBar>
         )
     }
 }
--- src/components/richText.js
+++ src/components/richText.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 export default class extends React.Component{
     render() {
-
-        let createMarkup = () => {
-            return {__html: this.props.data.text};
-        };
-
         return(
             <div>
                 <h2>{this.props.data.title}</h2>
-                <div dangerouslySetInnerHTML={createMarkup()}>
+                <div dangerouslySetInnerHTML={{__html: this.props.data.text}}>
                 </div>
             </div>
-            )
+        )
     }
 }
